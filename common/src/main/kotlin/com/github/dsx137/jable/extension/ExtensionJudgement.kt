007982b26ddf273ddb7e653c2fa96b6e878ae8c9
dsx137 <1419696375@qq.com>
2025-02-27 10:35:40 +0800

    chore: delete lib

--- common/src/main/kotlin/com/github/dsx137/jable/extension/ExtensionJudgement.kt
+++
@@ -1,8 +0,0 @@
-package com.github.dsx137.jable.extension
-
-fun Boolean.yes(predicate: () -> Unit): Boolean = this.also { if (it) predicate() }
-fun Boolean.no(predicate: () -> Unit): Boolean = this.also { if (!it) predicate() }
-
-infix fun Boolean.nor(boolean: Boolean): Boolean = !(this || boolean)
-infix fun Boolean.nand(boolean: Boolean): Boolean = !(this && boolean)
-infix fun Boolean.xnor(boolean: Boolean): Boolean = this == boolean
