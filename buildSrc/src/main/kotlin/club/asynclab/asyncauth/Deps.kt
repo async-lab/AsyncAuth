546751559d3ed48bd3f80e807018573d77415859
LittleStrange <1934944134@qq.com>
2025-11-14 20:38:21 +0800

    update 1.1.0 version

--- buildSrc/src/main/kotlin/club/asynclab/asyncauth/Deps.kt
+++ buildSrc/src/main/kotlin/club/asynclab/asyncauth/Deps.kt
@@ -5,5 +5,4 @@ import club.asynclab.asyncauth.api.IConstants
 object Deps : IConstants {
     const val HIKARI = "com.zaxxer:HikariCP:4.0.3"
     const val MYSQL = "mysql:mysql-connector-java:8.0.33"
-    const val JWT = "com.auth0:java-jwt:4.5.0"
 }
